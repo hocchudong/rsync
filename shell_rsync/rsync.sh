dd46a7a229221155baf39c698975079142afb9cb
trananhkma <trananhkma@gmail.com>
2014-11-24 17:41:15 +0700

    new file:   shell_rsync/config.cfg 	new file:   shell_rsync/rsync.sh

---
+++ shell_rsync/rsync.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source config.cfg
+
+mkdir -p /root/backup_data/{controller,compute,network}
+
+ssh-keygen -R root@$CONTROLLER
+ssh-keygen -R root@$COMPUTE
+ssh-keygen -R root@$NETWORK
+
+sshpass -p $PASSCONT rsync -avz --delete -e ssh root@$CONTROLLER:$SRCCONT $DESTCONT
+sshpass -p $PASSCOM rsync -avz --delete -e ssh root@$COMPUTE:$SRCCOM $DESTCOM
+sshpass -p $PASSNET rsync -avz --delete -e ssh root@$NETWORK:$SRCNET $DESTNET
+
