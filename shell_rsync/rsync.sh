c8af13a7820a152275785db4ddaa4de4f7cbc4f7
Trần Tuấn Anh <trananhkma@users.noreply.github.com>
2014-11-25 16:23:20 +0700

    Update rsync.sh

--- shell_rsync/rsync.sh
+++ shell_rsync/rsync.sh
@@ -7,7 +7,8 @@ mkdir -p /root/backup_data/{controller,compute,network}
 #ssh-keygen -R root@$COMPUTE
 #ssh-keygen -R root@$NETWORK
 
+apt-get install sshpass
+
 sshpass -p $PASSCONT rsync -avz --delete -e ssh root@$CONTROLLER:$SRCCONT $DESTCONT
 sshpass -p $PASSCOM rsync -avz --delete -e ssh root@$COMPUTE:$SRCCOM $DESTCOM
 sshpass -p $PASSNET rsync -avz --delete -e ssh root@$NETWORK:$SRCNET $DESTNET
-
